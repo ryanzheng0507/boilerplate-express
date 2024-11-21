8cb13646d99377999bb1e2d5fb11c9223fbbe534
ryanzheng0507 <ryanzheng0507@gmail.com>
2024-11-21 01:49:09 -0800

    feat: Use the app.get() method to serve the string 'Hello Express' to GET requests matching the /(root) path

--- myApp.js
+++ myApp.js
@@ -3,6 +3,17 @@ let app = express();
 
 console.log("Hello World")
 
+// In Express, routes takes the following structure:
+// app.METHOD(PATH, HANDLER).
+// METHOD is an http method in lowercase.
+// PATH is a relative path on the server(it can be a string, or even a regular expression).
+// HANDLER is a function that Express calls when the route is matched.
+// Handlers take the form function(req, res) {...}, where req is the request object, and res is the response object.
+// Use the app.get() method to serve the string "Hello Express" to GET requests matching the /(root) path.
+app.get('/', (req, res) => {
+  res.send('Hello Express')
+})
+
 
 
 
