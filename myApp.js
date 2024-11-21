057392751a99dd8f145333fe669b7b1f03bbcc2f
ryanzheng0507 <ryanzheng0507@gmail.com>
2024-11-21 02:32:01 -0800

    feat: Serve the object {'message': 'Hello json'} as a response, in JSON format, to GET requests to the /json route

--- myApp.js
+++ myApp.js
@@ -32,7 +32,31 @@ app.get('/', (req, res) => {
 // A middleware needs to be mounted using the method app.use(path, middlewareFunction).
 // The first path argument is optional.
 // If you don't pass it, the middleware will be executed for all requests.
+// The absolute path to the assets folder is __dirname + /public.
 app.use('/public', express.static(__dirname + '/public'))
+
+// While an HTML server serves HTML, an API serves data.
+// A REST(REpresentational State Transfer) API allows data exchange in a simple way,
+// without the need for clients to know any detail about the server.
+// The client only needs to know where the resource is(the URL),
+// and the action it wants to perform on it(the verb).
+// The GET verb is used when you are fetching some information, without modifying anything.
+// These days, the preferred data format for moving information around the web is JSON.
+// Simply put, JSON is a convenient way to represent a JavaScript object as a string, so it can be easily transmitted.
+// Let's create a simple API by creating a route that responds with JSON at the path /json.
+// You can do it as usual, with the app.get() method.
+// Inside the route handler, use the method res.json(), passing in an object as an argument.
+// This method closes the request-response loop, returning the data.
+// Behind the scenes, it converts a vlid JavaScript object into a string,
+// then sets the appropriate headers to tell your browser that you are serving JSON,
+// and sends the data back.
+// A valid object ahs the usual structure {key: data}.
+// data can be a number, a string, a nested object or an array.
+// data can also be a variable or the result off a function call,
+// in which case it will be evaluated before being converted into a string.
+app.get('/json', (req, res) => {
+  res.json({"message": "Hello json"})
+})
 
 
 
